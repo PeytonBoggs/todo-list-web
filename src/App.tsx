abb919534fd45cf3469199cc97fa32d856b70754
Peyton Boggs <peytonboggs@RTSLabss-MacBook-Pro.local>
2023-07-06 13:32:05 -0400

    Feature: getting tasks from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import { useState, useEffect } from 'react';
+import { Task } from './Task-Interface';
+
 function App() {
+    const [taskList, setTaskList] = useState<Task[]>([{id: 1, title: "wake up", complete: true}]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/tasks?id=&title=&complete=")
+        .then(response => response.json())
+        .then(data => {
+            setTaskList(data);
+        });
+    }, []);
+
+    function handleComplete(complete: boolean): string {
+        if (complete) {
+            return "Completed"
+        } else {
+            return "Not complete"
+        }
+    }
+
     return (
-        <h1>Todo List</h1>
+        <div>
+            <h1>Todo List</h1>
+            <div>
+                {taskList.map(task => (
+                <p key={task.id}>#{task.id}: {task.title} - {handleComplete(task.complete)}</p>    
+                ))}
+            </div>
+        </div>
     )
 }
 
